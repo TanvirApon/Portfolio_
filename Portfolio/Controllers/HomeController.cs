4f227f466eb376ae4e2dc232868352cd55b05638
tanvirhossainapon <tanvirhossainapon@gmail.com>
2024-01-29 13:17:23 +0600

    Basic Structures Created

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Education Page.";
+
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Project page.";
+
+            return View();
+        }
+
+        public ActionResult Refference()
+        {
+            ViewBag.Message = "Refference  page.";
+
+            return View();
+        }
+
+        public ActionResult Personalinfo()
+        {
+            ViewBag.Message = "Personal Information page.";
+
+            return View();
+        }
+    }
+}
